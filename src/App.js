997d90e11e599b64852c9493e8779f1277c4014c
浮中有夸 <fzyk.dylj@foxmail.com>
2022-12-12 23:39:22 +0800

    基础配置

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Layout from '@/pages/Layout'
+import { Button } from 'antd'
+//@ 是别名的配置
+import Login from '@/pages/Login'
 
 function App () {
   return (
-    <div className="App">
+    //路由配置
+    <BrowserRouter>
+      <div className="App">
+        <Button type="primary">Primary</Button>
+        <Routes>
+          {/* 创建路由path 和组件的对应关系 */}
+          <Route path='/' element={<Layout></Layout>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
 
-    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+//导入index文件
+import './index.scss'
+//导入antd文件 教的是要导入 反而出错 不导入还不出错
+//import 'antd/dist/antd.min.css'
 
 import App from './App'
 
